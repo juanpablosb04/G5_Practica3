d5d8cbd314bca2f5fb718beacd2a5656f325d0ac
juanpablosb04 <juanpablosb04@hotmail.com>
2025-08-07 21:25:50 -0600

    Base del proyecto en Visual, Model, Base de datos y Carga a la tabla de Consulta
    La tabla ya carga en orden ascendente con prioridad al Estado "Pendiente"

--- G5_Practica3/Controllers/HomeController.cs
+++ G5_Practica3/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using G5_Practica3.EF;
+using G5_Practica3.Models;
 
 namespace G5_Practica3.Controllers
 {
@@ -13,16 +15,32 @@ namespace G5_Practica3.Controllers
             return View();
         }
 
-        public ActionResult Consulta()
+        public ActionResult Registro()
         {
 
             return View();
         }
 
-        public ActionResult Registro()
-        {
 
-            return View();
+        public ActionResult Lista()
+        {
+            using (var dbContext = new G5_Practica3Entities())
+            {
+                var lista = dbContext.Principal
+                .OrderByDescending(p => p.Estado == "Pendiente")  
+                .ThenBy(p => p.ID_Compra)
+                .Select(p => new Models.Principal
+                {
+                    ID_Compra = p.ID_Compra,
+                    Descripcion = p.Descripcion,
+                    Precio = p.Precio,
+                    Saldo = p.Saldo,
+                    Estado = p.Estado
+                })
+                .ToList();
+
+                return View(lista);
+            }
         }
     }
 }
---
+++ G5_Practica3/Models/Principal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace G5_Practica3.Models
+{
+    public class Principal
+    {
+        public int ID_Compra { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Saldo { get; set; }
+        public string Estado { get; set; }
+    }
+}
