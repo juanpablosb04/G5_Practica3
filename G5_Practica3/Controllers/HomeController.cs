65a32c7d9fba8463b5f537cf5215741f24bbb15b
juanpablosb04 <juanpablosb04@hotmail.com>
2025-08-07 20:12:40 -0600

    Initial commit

---
+++ G5_Practica3/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace G5_Practica3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Consulta()
+        {
+
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+
+            return View();
+        }
+    }
+}
