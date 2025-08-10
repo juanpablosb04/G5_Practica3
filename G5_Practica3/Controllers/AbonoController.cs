ae06b6bb6c695cd8e0f9e144c4adf1907963457d
Robert Quesada Borbon <rquesada20345@ufide.ac.cr>
2025-08-09 22:25:24 -0600

    Tarea completa

--- G5_Practica3/Controllers/AbonoController.cs
+++ G5_Practica3/Controllers/AbonoController.cs
@@ -1,14 +1,19 @@
-﻿using System;
+﻿using G5_Practica3.EF;
+using G5_Practica3.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using G5_Practica3.EF;
 
 namespace G5_Practica3.Controllers
 {
     public class AbonoController : Controller
     {
+
+
+        [HttpGet]
         public ActionResult Registro()
         {
             using (var db = new PracticaS12Entities())
@@ -27,17 +32,60 @@ namespace G5_Practica3.Controllers
 
         }
 
-        public JsonResult ObtenerSaldo(int idCompra)
+        [HttpPost]
+        public ActionResult RegistrarAbono(EF.Abonos abono)
         {
-            using (var db = new PracticaS12Entities())
+            using (var dbContext = new PracticaS12Entities())
             {
-                var saldo = db.Principal
-                              .Where(c => c.Id_Compra == idCompra)
-                              .Select(c => c.Saldo)
-                              .FirstOrDefault();
+                decimal saldoPendiente = dbContext.Principal
+                    .Where(c => c.Id_Compra == abono.Id_Compra)
+                    .Select(c => c.Saldo)
+                    .FirstOrDefault();
 
-                return Json(saldo, JsonRequestBehavior.AllowGet);
+                decimal precioProducto = dbContext.Principal
+                    .Where(c => c.Id_Compra == abono.Id_Compra)
+                    .Select(c => c.Precio)
+                    .FirstOrDefault();
+
+
+                if (abono.Monto > 0 && saldoPendiente > 0 && abono.Monto <= saldoPendiente)
+                {
+                    abono.Fecha = DateTime.Now;
+                    dbContext.Abonos.Add(abono);
+
+                    var compra = dbContext.Principal.First(c => c.Id_Compra == abono.Id_Compra);
+                    compra.Saldo -= abono.Monto;
+                    
+                    if (compra.Saldo == 0)
+                    {
+                        compra.Saldo = 0;
+                        compra.Estado = "Pagado";
+                    }
+
+                    dbContext.SaveChanges();
+                }
+                else
+                {
+                    TempData["Mensaje"] = "No se pudo registrar el abono. Digite un monto correcto";
+                    return RedirectToAction("Registro", "Abono");
+                }
+            }
+
+            TempData["Mensaje"] = "Abono registrado correctamente";
+            return RedirectToAction("Index", "Home");
+        }
+        public JsonResult ObtenerSaldoRestante(int idCompra)
+        {
+            using (var dbContext = new PracticaS12Entities())
+            {
+                decimal saldoPendiente = dbContext.Principal
+                    .Where(c => c.Id_Compra == idCompra)
+                    .Select(c => c.Saldo)
+                    .FirstOrDefault();
+
+                return Json(new { saldoPendiente }, JsonRequestBehavior.AllowGet);
             }
         }
+
     }
 }
